a61c592b5fd6a2309f8e5d4ef5f9107c389f94fa
Annie983284450-1 <azhou60@gatech.edu>
2024-10-23 18:05:34 -0400

    Update tune_realworld.py

--- tune_realworld.py
+++ tune_realworld.py
@@ -19,7 +19,7 @@ parser.add_argument('--data_types', nargs='+', type=str, default=['sepsis'])
 parser.add_argument('--algo_groups', nargs='+', type=str, default=['ApproxNeuraLCB_cp'])
 parser.add_argument('--policies', nargs='+', type=str, default=['eps-greedy'])
 parser.add_argument('--num_sim', type=int, default=1)
-parser.add_argument('--models_per_gpu', type=int, default=3)
+parser.add_argument('--models_per_gpu', type=int, default=1)
 parser.add_argument('--gpus', nargs='+', type=int, default=[0], help='gpus indices used for multi_gpu')
 
 parser.add_argument('--result_dir', type=str, default='results/stock_d=21_a=8_pi=eps-greedy0.1_std=0.1', help='result directory for collect_results()')
@@ -133,53 +133,7 @@ def multi_gpu_launcher_linux_mac(commands,gpus,models_per_gpu):
         if p is not None:
             p.wait()
 
-
  
-
- 
-# hyper_mode = 'best' # ['full', 'best']
-# if hyper_mode == 'full':
-#     # Grid search space: used for grid search in the paper
-#     lr_space = [1e-4,1e-3]
-#     train_mode_space = [(1,1,1),(50,100,-1)]
-#     beta_space = [0.01, 0.05, 1,5,10]
-#     rbfsigma_space = [1] #[0.1, 1,10]
-# elif hyper_mode == 'best':
-#     # The best searc sapce inferred fro prior experiments 
-#     lr_space = [1e-4]
-#     train_mode_space = [(1,1,1)]
-#     beta_space = [1]
-#     rbfsigma_space = [10] #10 for mnist, 0.1 for mushroom
- 
-# def create_commands(data_type='mushroom', algo_group='approx-neural', num_sim=3, policy='eps-greedy'):
-#     commands = []
-#     if algo_group == 'approx-neural':
-#         for lr in lr_space:
-#             for batch_size,num_steps,buffer_s in train_mode_space:
-#                 for beta in beta_space:
-#                     commands.append('python realworld_main.py --data_type {} --algo_group {} --num_sim {} --batch_size {} --num_steps {} --buffer_s {} --beta {} --lr {} --policy {}'.format(data_type,algo_group,num_sim,batch_size,num_steps,buffer_s,beta,lr,policy))
-
-#     elif algo_group == 'neurallinlcb':
-#         for beta in beta_space:
-#             commands.append('python realworld_main.py --data_type {} --algo_group {} --num_sim {}  --beta {} --policy {}'.format(data_type,algo_group,num_sim,beta,policy))
-
-    
-#     elif algo_group == 'neural-greedy':
-#         for lr in lr_space:
-#             for batch_size,num_steps,buffer_s in train_mode_space:
-#                 commands.append('python realworld_main.py --data_type {} --algo_group {} --num_sim {} --batch_size {} --num_steps {} --buffer_s {} --lr {} --policy {}'.format(data_type,algo_group,num_sim,batch_size,num_steps,buffer_s,lr,policy))
-
-#     elif algo_group == 'kern':
-#         for rbf_sigma in rbfsigma_space:
-#             commands.append('python realworld_main.py --data_type {} --algo_group {} --num_sim {} --rbf_sigma {} --policy {}'.format(data_type,algo_group,num_sim,rbf_sigma,policy))
-
-#     elif algo_group == 'baseline': # no tuning 
-#         commands.append('python realworld_main.py --data_type {} --algo_group {} --num_sim {} --policy {}'.format(data_type,algo_group,num_sim,policy))
-
-#     else:
-#         raise NotImplementedError
-
-#     return commands
 def create_commands(data_type='sepsis', algo_group='ApproxNeuraLCB_cp', num_sim=1, policy='eps-greedy'):
     # hyper_mode = 'best' # ['full', 'best']
     test = True
